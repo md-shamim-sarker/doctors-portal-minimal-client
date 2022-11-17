448e4e4ac63b29b9724535150f99ca62220bf408
shamimspro <shamimspro@gmail.com>
2022-11-17 21:13:11 +0600

    firebase installation

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            Login page
+        <div className="hero min-h-screen bg-base-200">
+            <div className="w-4/5 mx-auto hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input name='email' type="email" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input name='password' type="password" placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <button className="label-text-alt link link-hover">Forgot password?</button>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                            <div className="divider">OR</div>
+                            <button className="btn btn-primary">Login With Google</button>
+                            <div>You don't have an account? <Link className='underline' to={"/register"}>Register</Link></div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Login/Register.js
+++ src/pages/Login/Register.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            Register page
+        <div className="hero min-h-screen bg-base-200">
+            <div className="w-4/5 mx-auto hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Register now!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Full Name</span>
+                            </label>
+                            <input name='fullName' type="text" placeholder="Full Name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input name='email' type="email" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input name='password' type="password" placeholder="password" className="input input-bordered" />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Register</button>
+                        </div>
+                        <div>You already have an account? <Link className='underline' to={"/login"}>Login</Link></div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
